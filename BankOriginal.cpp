bf859948d43eab3a04cd4a2aaa69dbba5b14e1af
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2024-11-28 19:13:33 +0600

    s

--- BankOriginal.cpp
+++ BankOriginal.cpp
@@ -75,9 +75,9 @@ if(num==23303106){
 else {
     cout<<"Not found.Try again\n";
     count++;
-   
     if(count==2){
         cout<<"Please sign up first";
+
         cout<<"Are you interested to create an account?\n yes/no\n";
         cin>>op;
         if(op=="yes"||"Yes"){
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+ cout<<"Please sign up first";
+        cout<<"Are you interested to create an account?\n yes/no\n";
+        cin>>op;
+        if(op=="yes"||"Yes"){
+            sing_up();
